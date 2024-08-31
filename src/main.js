a12b6b9c592f1a8b969c55858a88ce9a21b5671f
u-masao <4973920+u-masao@users.noreply.github.com>
2024-08-31 09:24:50 +0900

    bugfix, add dt param

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 window.onload = function() {
 
-  body = document.body;
-
   var width= 300;
   var height = 300;
+  const body = document.body;
 
   // append canvas
   init_canvas(body, width, height);
+
+  // append console
+  init_console(body, width, height);
 
   // append widgets for parameter control
   init_widgets(body);
@@ -22,6 +24,20 @@ function init_canvas(parent, width, height) {
   canvas.setAttribute('height', String(height) + 'px');
   canvas.innerText = 'draw points with javascript';
   parent.appendChild(canvas);
+}
+
+function init_console(parent, width, height) {
+  const console = document.createElement('div');
+  console.id = 'console';
+  parent.appendChild(console);
+}
+
+function log(message) {
+  const console = document.getElementById('console');
+  const p = document.createElement('p');
+  p.innerText = message;
+  console.appendChild(p);
+  console.scrollTop = console.scrollHeight;
 }
 
 function add_slider(id_value, min_value , max_value, step_value, parent) {
@@ -75,17 +91,17 @@ function order(theta) {
   var r = 0.0;
   var n = theta.length;
   for (let i = 0; i < n; i++) {
-    r += Math.abs(Math.sin(theta[i]) / n);
+    r += Math.sin(theta[i]) / n;
   }
-  return r;
+  return Math.abs(r);
 }
 
 function init_widgets(parent) {
   const control_panel = document.createElement('div');
-  add_slider('n', 3, 10, 1.0, control_panel);
-  add_slider('k', 3, 10, 1.0, control_panel);
-  add_slider('omega_sd', 0, 1, 0.05, control_panel);
-  add_slider('j', 0, 4, 0.05, control_panel);
+  add_slider('n', 3, 20, 1.0, control_panel);
+  add_slider('k', 3, 20, 1.0, control_panel);
+  add_slider('omega_sd', 0, 0.3, 0.05, control_panel);
+  add_slider('j', 0, 1, 0.05, control_panel);
   parent.appendChild(control_panel);
 }
 
@@ -95,12 +111,22 @@ function update_canvas(theta, width, height) {
   ctx.fillStyle = 'green';
   ctx.fillRect(0,0,width,height);
 
-  ctx.fillStyle = 'red';
   for (let i = 0; i < theta.length; i++) {
     var x = width / 2 + width / 3 * Math.cos(2 * Math.PI * i / theta.length);
     var y = height / 2 + height / 3 * Math.sin(2 * Math.PI * i / theta.length);
-    var radius = 5 * (theta[i] % Math.PI) + 1;
+    var radius = 10 * (Math.sin(theta[i]) + 1) / 2 + 5  ;
 
+    ctx.fillStyle = 'red';
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, 2 * Math.PI);
+    ctx.closePath();
+    ctx.fill();
+
+    var x = width / 2 + width / 4 * Math.cos(theta[i]);
+    var y = height / 2 + height / 4 * Math.sin(theta[i]);
+    var radius = 5;
+
+    ctx.fillStyle = 'blue';
     ctx.beginPath();
     ctx.arc(x, y, radius, 0, 2 * Math.PI);
     ctx.closePath();
@@ -111,34 +137,34 @@ function update_canvas(theta, width, height) {
 function main(width, height) {
 
   // init params
+  var omega_mu = 0.2;
+  var omega_sigma = 0.0;
   var n = parseInt( document.getElementById('n').value, 10);
   var k = parseInt( document.getElementById('k').value, 10);
+  var dt = 0.01;
+  var limit = 0;
+  var interval_ms = 100;
+
+  // init values
   var omega = Array(n);
   var theta = Array(n);
   for (let i = 0; i < n; i++) {
-    omega[i] = rnorm(Math.PI * 2, Math.PI * 0.0);
+    omega[i] = rnorm(omega_mu * Math.PI * 2, 2 * Math.PI * omega_sigma);
     theta[i] = Math.random() * 2 * Math.PI;
   }
 
-  var limit = 1000;
-  var interval_ms = 100;j
-
-  console.log({n});
-  console.log({k});
-  console.log({omega});
-
-  var counter= 0;
+  // main loop
+  var counter = 0;
   const interval = setInterval(function() {
+    // log(counter + ": " + theta[0]/(2*Math.PI));
     m = order(theta);
+    log(counter + ": " + m);
     theta_dt = kuramoto_formula(omega, k, n, theta);
-    console.log({theta});
-    console.log({m});
-    console.log({theta_dt});
     for (let j = 0; j < n; j++) {
-      theta[j] += theta_dt[j];
+      theta[j] += theta_dt[j] * dt;
     }
     update_canvas(theta, width, height);
-    if (counter >= limit) {
+    if (limit != 0 && counter >= limit) {
         clearInterval(interval);
     }
     counter ++;
