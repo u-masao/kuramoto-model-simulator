737a853d4668849c0887db4bf639624c46b2d496
u-masao <4973920+u-masao@users.noreply.github.com>
2024-09-04 00:03:01 +0900

    code refact

--- src/main.c
+++ src/main.c
@@ -30,10 +30,16 @@ void kuramoto_formula_fast(int n, double k, double *omega, double *theta,
   }
 }
 
-void kuramoto_simulation(int n, double k, double *omega, double *theta,
-                         int loop_count, double time_delta) {
+void simulation(int n, double k, double *omega, double *theta, int loop_count,
+                double time_delta) {
   double *theta_dt;
   theta_dt = (double *)calloc(n, sizeof(double));
+
+  printf("==== initial variables\n");
+  printParams(n, omega, theta);
+
+  printf("==== simulation\n");
+
   for (int i = 0; i < loop_count; i++) {
     double com_x;
     double com_y;
@@ -46,7 +52,8 @@ void kuramoto_simulation(int n, double k, double *omega, double *theta,
       R = 1.0;
     }
     Theta = atan2(com_y, com_x);
-    printf("R: %f, Theta: %f, com_x: %f, com_y: %f\n", R, Theta, com_x, com_y);
+    printf("Step: %d, R: %f, Theta: %f, com_x: %f, com_y: %f\n", i, R, Theta,
+           com_x, com_y);
     kuramoto_formula_fast(n, k, omega, theta, R, Theta, theta_dt);
     for (int j = 0; j < n; j++) {
       theta[j] += theta_dt[j] * time_delta;
@@ -57,6 +64,7 @@ void kuramoto_simulation(int n, double k, double *omega, double *theta,
 double frand() { return (double)rand() / ((double)RAND_MAX + 1); }
 
 double frand_mt(sfmt_t *sfmt) { return sfmt_genrand_real2(sfmt); }
+
 double rnorm(double mu, double sigma) {
   return mu + sigma * sqrt(-2.0 * log(frand())) * cos(2.0 * PI * frand());
 }
@@ -66,22 +74,10 @@ double rnorm_mt(sfmt_t *sfmt, double mu, double sigma) {
                   cos(2.0 * PI * frand_mt(sfmt));
 }
 
-int main(int argc, char const *argv[]) {
-  int n = 30;
-  double k = 4;
-  double mu = 1.0;
-  double sigma = 1.0;
-  double time_delta = 0.01;
-  int loop_count = 1000;
-  unsigned int seed = 0;
-
-  double *omega;
-  double *theta;
-  omega = (double *)calloc(n, sizeof(double));
-  theta = (double *)calloc(n, sizeof(double));
-
+void init_variables(const int n, const double mu, const double sigma,
+                    const unsigned int seed, double *omega, double *theta) {
+  // setup random
   sfmt_t sfmt;
-  seed = (unsigned int)time(NULL);
   sfmt_init_gen_rand(&sfmt, seed);
   // srand(seed);
 
@@ -91,8 +87,38 @@ int main(int argc, char const *argv[]) {
     // omega[i] = rnorm(&sfmt, mu, sigma);
     // theta[i] = 2.0 * PI * frand_mt(&sfmt);
   }
-  printParams(n, omega, theta);
-  kuramoto_simulation(n, k, omega, theta, loop_count, time_delta);
+}
 
+void kuramoto_model_simulator(const int n, const double k,
+                              const double time_delta, const int loop_count,
+                              const double mu, const double sigma,
+                              const unsigned int seed) {
+  // variables
+  double *omega;
+  double *theta;
+
+  omega = (double *)calloc(n, sizeof(double));
+  theta = (double *)calloc(n, sizeof(double));
+
+  // init variables
+  init_variables(n, mu, sigma, seed, omega, theta);
+
+  // run simulation
+  simulation(n, k, omega, theta, loop_count, time_delta);
+}
+
+int main(int argc, char const *argv[]) {
+  // simulation condition
+  const int n = 30;
+  const double k = 4;
+  const double time_delta = 0.01;
+  const int loop_count = 1000;
+  const double mu = 1.0;
+  const double sigma = 1.0;
+  unsigned int seed;
+
+  seed = (unsigned int)time(NULL);
+
+  kuramoto_model_simulator(n, k, time_delta, loop_count, mu, sigma, seed);
   return 0;
 }
