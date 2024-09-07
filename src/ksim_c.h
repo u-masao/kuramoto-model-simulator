be811f6a4af64fbebf67aa0437018794ac88bb99
u-masao <4973920+u-masao@users.noreply.github.com>
2024-09-07 13:40:11 +0000

    add ksim_c.h header file

---
+++ src/ksim_c.h
@@ -0,0 +1,6 @@
+void kuramoto_model_simulator_c(const int n, const double k,
+                              const double time_delta, const int loop_count,
+                              const double mu, const double sigma,
+                              const unsigned int seed, double *omega,
+                              double *theta, double *com_x, double *com_y,
+                              int verbose);
