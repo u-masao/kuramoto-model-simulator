fe26305af4e4535540adb1d202a394a62c845612
u-masao <4973920+u-masao@users.noreply.github.com>
2024-09-09 22:27:43 +0900

    grid generate

--- src/optimize.py
+++ src/optimize.py
@@ -20,46 +20,6 @@ def score_function(
     )
 
 
-def grid(kwargs):
-    k_c = 2.76
-    time_delta = 0.01
-    loop_count = 100 * 20
-
-    result = []
-    for n in [10**x for x in range(4)]:
-        for k_ratio in [0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 2, 3, 4]:
-            k = k_c * k_ratio
-            print(n, k)
-            simulated = kuramoto_model_simulator(
-                KSIM_LIB,
-                n=n,
-                k=k,
-                time_delta=time_delta,
-                loop_count=loop_count,
-                mu=1.0,
-                sigma=1.0,
-                seed=0,
-                verbose=0,
-            )
-
-            com_x = np.array(simulated["com_x"])
-            com_y = np.array(simulated["com_y"])
-            r = np.sqrt(com_x**2 + com_y**2)
-
-            result.append(
-                {
-                    "n": n,
-                    "k": k,
-                    "r_mean": r.mean(),
-                    "r_std": r.std(),
-                }
-            )
-
-    result_df = pd.DataFrame(result)
-    result_df.to_csv(kwargs["output_filepath"])
-    print(result_df.sort_values(["k", "n"]))
-
-
 def objective_wrapper(
     r_mu: float = 0.4,
     r_sigma: float = 0.3,
@@ -101,45 +61,64 @@ def objective_wrapper(
     return objective
 
 
-def plot_com(df, output_filepath, plot_alpha: float = 0.03):
+class MatrixPlot:
+    def __init__(
+        self,
+        bgcolor: str = "#111111",
+        color: str = "#9999ff",
+        plot_alpha: float = 0.03,
+        width: int = 1,
+        height: int = 1,
+    ):
 
-    bgcolor = "#111111"
-    color = "#9999ff"
-    fig, ax = plt.subplots(1, 1, figsize=(8, 8), facecolor=bgcolor)
-    ax.scatter(
-        df["com_x"],
-        df["com_y"],
-        facecolors=color,
-        alpha=plot_alpha,
-        s=20,
-        edgecolors="none",
-    )
-    ax.tick_params(
-        labelbottom=False, labelleft=False, labelright=False, labeltop=False
-    )
-    ax.tick_params(bottom=False, left=False, right=False, top=False)
-    ax.set_facecolor(bgcolor)
-    for x in fig.gca().spines:
-        fig.gca().spines[x].set_visible(False)
-    fig.tight_layout()
-    fig.savefig(output_filepath, dpi=300)
+        self.width = width
+        self.height = height
+        self.bgcolor = bgcolor
+        self.color = color
+        self.plot_alpha = plot_alpha
+
+        self.fig, ax = plt.subplots(
+            width, height, figsize=(height * 2, width * 2), facecolor=bgcolor
+        )
+
+        if width * height == 1:
+            ax = [[ax]]
+        self.ax = ax.reshape(width, height)
+
+    def plot(self, df, x, y, title: str, size: float = 10):
+        if x >= self.width or y >= self.height:
+            raise ValueError("x or y too large")
+
+        ax = self.ax[x, y]
+
+        ax.scatter(
+            df["com_x"],
+            df["com_y"],
+            facecolors=self.color,
+            alpha=self.plot_alpha,
+            s=size,
+            edgecolors="none",
+        )
+        ax.set_facecolor(self.bgcolor)
+        ax.tick_params(
+            labelbottom=False,
+            labelleft=False,
+            labelright=False,
+            labeltop=False,
+        )
+        ax.tick_params(bottom=False, left=False, right=False, top=False)
+        ax.set_aspect("equal", "box")
+        ax.axis("off")
+        # ax.set_title(title, color=self.color)
+
+    def save(self, output_filepath: str):
+        self.fig.tight_layout()
+        self.fig.savefig(output_filepath, dpi=300)
 
 
-@click.command()
-@click.argument("output_filepath", type=click.Path())
-@click.option(
-    "--ksim_library_path", type=click.Path(exists=True), default="./ksim.so"
-)
-@click.option("--time", type=int, default=120)
-@click.option("--optuna_seed", type=int, default=0)
-@click.option("--seed", type=int, default=0)
-def main(**kwargs):
-    n = 30
+def search(optuna_seed, seed, n, loop_sec):
     time_delta = 0.01
-    loop_count = 100 * kwargs["time"]
-    optuna_seed = kwargs["optuna_seed"]
-    seed = kwargs["seed"]
-
+    loop_count = 100 * loop_sec
     optuna.logging.set_verbosity(optuna.logging.WARNING)
     study = optuna.create_study(
         direction="minimize",
@@ -185,12 +164,57 @@ def main(**kwargs):
             "r": r,
         }
     )
-    df.to_csv("../data/best_params.csv")
 
-    print(f"R mean: {df['r'].mean()}")
-    print(f"R std: {df['r'].std()}")
+    return df
 
-    plot_com(df, "../data/figures/best_params.png")
+
+@click.command()
+@click.argument("output_filepath", type=click.Path())
+@click.option(
+    "--ksim_library_path", type=click.Path(exists=True), default="./ksim.so"
+)
+@click.option("--time", type=int, default=120)
+@click.option("--optuna_seed", type=int, default=0)
+@click.option("--seed", type=int, default=0)
+@click.option("--n", type=int, default=10)
+@click.option("--width", type=int, default=10)
+@click.option("--height", type=int, default=10)
+def main(**kwargs):
+    n = kwargs["n"]
+    width = kwargs["width"]
+    height = kwargs["height"]
+
+    mp = MatrixPlot(width=width, height=height)
+    y = 0
+    seed = 0
+    while y < height:
+        x = 0
+        optuna_seed = 0
+        new_line = False
+        while x < width:
+            print(f"seed: {seed}, optuna_seed: {optuna_seed}")
+            df = search(optuna_seed, seed, n, kwargs["time"])
+            r_mean = df["r"].mean()
+            r_std = df["r"].std()
+            print(f"R mean: {r_mean}, std: {r_std}")
+            if r_mean > 0.6 or r_std < 0.1:
+                print("break loop")
+                break
+            mp.plot(
+                df,
+                x=x,
+                y=y,
+                title=f"s:{seed}, o:{optuna_seed}",
+                size=3,
+            )
+            x += 1
+            optuna_seed += 10
+            new_line = True
+        seed += 10
+        if new_line:
+            y += 1
+
+    mp.save("../data/figures/best_params.png")
 
 
 if __name__ == "__main__":
