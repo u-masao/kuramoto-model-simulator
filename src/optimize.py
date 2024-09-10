6d6a2e939b3e8c889e1bd091f7627042d960a88f
u-masao <4973920+u-masao@users.noreply.github.com>
2024-09-10 09:40:36 +0900

    add param search code

--- src/optimize.py
+++ src/optimize.py
@@ -1,3 +1,6 @@
+import sqlite3
+from pathlib import Path
+
 import click
 import matplotlib.pyplot as plt
 import numpy as np
@@ -15,6 +18,9 @@ def score_function(
     r_mu: float = 0.0,
     r_sigma: float = 1.0,
 ) -> float:
+    '''
+    パラメーター探索用のスコア定義
+    '''
     return (
         score_weight_mean * (r.mean() - r_mu) ** 2 + (r.std() - r_sigma) ** 2
     )
@@ -23,6 +29,7 @@ def score_function(
 def objective_wrapper(
     r_mu: float = 0.4,
     r_sigma: float = 0.3,
+    score_weight_mean: float = 0.1,
     loop_count: int = 6000,
     time_delta: float = 0.01,
     n: int = 30,
@@ -32,12 +39,19 @@ def objective_wrapper(
     verbose: int = 0,
     k_min: float = 0.5,
     k_max: float = 10,
-    score_weight_mean: float = 0.1,
 ):
+    '''
+    最適化関数のジェネレーター
+    '''
     def objective(trial):
+        '''
+        探索範囲と最適化関数の定義
+        '''
 
+        # 探索範囲の定義
         k = trial.suggest_float("k", k_min, k_max)
 
+        # シミュレーション
         simulated = kuramoto_model_simulator(
             KSIM_LIB,
             n=n,
@@ -50,6 +64,7 @@ def objective_wrapper(
             verbose=verbose,
         )
 
+        # スコア計算
         com_x = np.array(simulated["com_x"])
         com_y = np.array(simulated["com_y"])
         r = np.sqrt(com_x**2 + com_y**2)
@@ -58,10 +73,15 @@ def objective_wrapper(
             r, score_weight_mean=score_weight_mean, r_mu=r_mu, r_sigma=r_sigma
         )
 
+    # 関数を返す
     return objective
 
 
 class MatrixPlot:
+    '''
+    軌跡画像を作るクラス
+    '''
+
     def __init__(
         self,
         bgcolor: str = "#111111",
@@ -69,6 +89,7 @@ class MatrixPlot:
         plot_alpha: float = 0.03,
         width: int = 1,
         height: int = 1,
+        cell_size: int = 4,
     ):
 
         self.width = width
@@ -78,14 +99,20 @@ class MatrixPlot:
         self.plot_alpha = plot_alpha
 
         self.fig, ax = plt.subplots(
-            width, height, figsize=(height * 2, width * 2), facecolor=bgcolor
+            width,
+            height,
+            figsize=(height * cell_size, width * cell_size),
+            facecolor=bgcolor,
         )
 
         if width * height == 1:
-            ax = [[ax]]
+            ax = np.array([[ax]])
         self.ax = ax.reshape(width, height)
 
     def plot(self, df, x, y, title: str, size: float = 10):
+        '''
+        plot
+        '''
         if x >= self.width or y >= self.height:
             raise ValueError("x or y too large")
 
@@ -109,22 +136,35 @@ class MatrixPlot:
         ax.tick_params(bottom=False, left=False, right=False, top=False)
         ax.set_aspect("equal", "box")
         ax.axis("off")
-        # ax.set_title(title, color=self.color)
+        ax.set_title(title, color=self.color, alpha=0.3, y=-0.12)
 
     def save(self, output_filepath: str):
+        '''
+        ファイル保存とリソース解放
+        '''
         self.fig.tight_layout()
         self.fig.savefig(output_filepath, dpi=300)
+        plt.clf()
+        plt.close()
 
 
 def search(optuna_seed, seed, n, loop_sec):
+    '''
+    探索方法の実行
+    '''
     time_delta = 0.01
     loop_count = 100 * loop_sec
+
+    # 探索定義
     optuna.logging.set_verbosity(optuna.logging.WARNING)
     study = optuna.create_study(
         direction="minimize",
         sampler=optuna.samplers.TPESampler(seed=optuna_seed),
     )
+
+    # 探索実施
     study.optimize(
+
         objective_wrapper(
             r_mu=0.5,
             r_sigma=0.3,
@@ -140,6 +180,7 @@ def search(optuna_seed, seed, n, loop_sec):
 
     k = best_params["k"]
 
+    # データ取得のための再シミュレーション
     simulated = kuramoto_model_simulator(
         KSIM_LIB,
         n=n,
@@ -165,11 +206,11 @@ def search(optuna_seed, seed, n, loop_sec):
         }
     )
 
-    return df
+    return df, best_params
 
 
 @click.command()
-@click.argument("output_filepath", type=click.Path())
+@click.argument("database_filepath", type=click.Path())
 @click.option(
     "--ksim_library_path", type=click.Path(exists=True), default="./ksim.so"
 )
@@ -177,44 +218,92 @@ def search(optuna_seed, seed, n, loop_sec):
 @click.option("--optuna_seed", type=int, default=0)
 @click.option("--seed", type=int, default=0)
 @click.option("--n", type=int, default=10)
-@click.option("--width", type=int, default=10)
-@click.option("--height", type=int, default=10)
+@click.option("--limit", type=int, default=2)
+@click.option("--image_dir", type=str, default="images")
 def main(**kwargs):
+    '''
+    パラメーター探索、シミュレーション、軌跡の保存
+    '''
+
+    # ディレクトリ作成
+    save_dir = Path(kwargs["database_filepath"])
+    save_dir.parent.mkdir(parents=True, exist_ok=True)
+    (save_dir.parent / kwargs["image_dir"]).mkdir(parents=True, exist_ok=True)
+
+    # DB 準備
+    con = sqlite3.connect(kwargs["database_filepath"])
+    cur = con.cursor()
+    cur.execute(
+        "create table if not exists "
+        "ksim(seed, optuna_seed, k, n, skip, image_path)"
+    )
+
+    # 初期パラメーター設定
     n = kwargs["n"]
-    width = kwargs["width"]
-    height = kwargs["height"]
-
-    mp = MatrixPlot(width=width, height=height)
-    y = 0
     seed = 0
-    while y < height:
-        x = 0
-        optuna_seed = 0
-        new_line = False
-        while x < width:
-            print(f"seed: {seed}, optuna_seed: {optuna_seed}")
-            df = search(optuna_seed, seed, n, kwargs["time"])
-            r_mean = df["r"].mean()
-            r_std = df["r"].std()
-            print(f"R mean: {r_mean}, std: {r_std}")
-            if r_mean > 0.6 or r_std < 0.1:
-                print("break loop")
-                break
-            mp.plot(
-                df,
-                x=x,
-                y=y,
-                title=f"s:{seed}, o:{optuna_seed}",
-                size=3,
-            )
-            x += 1
-            optuna_seed += 10
-            new_line = True
-        seed += 10
-        if new_line:
-            y += 1
+    optuna_seed = 0
+    counter = 0
 
-    mp.save("../data/figures/best_params.png")
+    # 探索ループ
+    while counter < kwargs["limit"]:
+
+        # 条件表示
+        print(
+            f"== count: {counter}, n: {n}, seed: {seed}, "
+            f"optuna_seed: {optuna_seed}"
+        )
+
+        # db にデータがあれば次の seed へ
+        res = cur.execute(
+            f"select skip from ksim where n={n} and seed = {seed} "
+            f"and optuna_seed = {optuna_seed}"
+        )
+        if res.fetchone() is not None:
+            print(res.fetchone())
+            print("  data exist in database")
+            seed += 10
+            continue
+
+        # 探索
+        df, best_params = search(optuna_seed, seed, n, kwargs["time"])
+        r_mean = df["r"].mean()
+        r_std = df["r"].std()
+        k = best_params["k"]
+        print(f"R mean: {r_mean}, std: {r_std}, k: {k}")
+
+        # 単純な軌跡は次の seed へ
+        if r_mean > 0.6 or r_std < 0.1:
+            print("  break loop too simple")
+            cur.execute(
+                f"""insert into ksim values
+            ({seed}, {optuna_seed},{k},{n}, true, '')
+        """
+            )
+            seed += 10
+            continue
+
+        # db にデータをストア
+        file_name = f"trajectory_n{n}_s{seed}_o{optuna_seed}.png"
+        cur.execute(
+            f"""insert into ksim values
+            ({seed}, {optuna_seed},{k},{n}, false, '{file_name}')
+        """
+        )
+        con.commit()
+
+        # 軌跡画像の作成と保存
+        mp = MatrixPlot(width=1, height=1)
+        mp.plot(
+            df,
+            x=0,
+            y=0,
+            title=f"n:{n}, s:{seed}, o:{optuna_seed}",
+            size=20,
+        )
+        mp.save(save_dir.parent / kwargs["image_dir"] / file_name)
+
+        seed += 10
+        counter += 1
 
 
 if __name__ == "__main__":
