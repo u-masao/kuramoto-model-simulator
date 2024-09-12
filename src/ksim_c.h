51be23a36a11b0079de527c26c44e368cf2adcc5
u-masao <4973920+u-masao@users.noreply.github.com>
2024-09-12 21:59:03 +0900

    change function interface, split initialize and simulation

--- src/ksim_c.h
+++ src/ksim_c.h
@@ -1,6 +1,3 @@
-void kuramoto_model_simulator_c(const int n, const double k,
-                              const double time_delta, const int loop_count,
-                              const double mu, const double sigma,
-                              const unsigned int seed, double *omega,
-                              double *theta, double *com_x, double *com_y,
-                              int verbose);
+void kuramoto_model_simulator_c(int n, double k, double *omega, double *theta,
+                               int loop_count, double time_delta, double *com_x,
+                               double *com_y);
--- src/ksim_c.py
+++ src/ksim_c.py
@@ -1,7 +1,33 @@
 import ctypes
 import json
+from typing import List
 
 import click
+import numpy as np
+
+"""
+double frand() { return (double)rand() / ((double)RAND_MAX + 1); }
+
+double rnorm(double mu, double sigma) {
+  return mu + sigma * sqrt(-2.0 * log(frand())) * cos(2.0 * M_PI * frand());
+}
+"""
+
+
+def sample_initial_values_from_normal_dist(
+    n: int = 10,
+    mu: float = 1.0,
+    sigma: float = 1.0,
+    seed: int = 0,
+):
+    # set random state
+    np.random.RandomState(seed=seed)
+
+    # generate initial values
+    omega = np.random.normal(loc=mu, scale=sigma, size=n).tolist()
+    theta = (np.random.rand(n) * 2 * np.pi).tolist()
+
+    return omega, theta
 
 
 def cast_double_p(param):
@@ -10,53 +36,68 @@ def cast_double_p(param):
 
 def kuramoto_model_simulator(
     library_path: str,
-    n=30,
-    k=4,
-    time_delta=0.01,
-    loop_count=10,
-    mu=1.0,
-    sigma=1.0,
-    seed=0,
-    verbose=0,
+    input_omega: List[float],
+    input_theta: List[float],
+    n: int = 10,
+    k: int = 4,
+    time_delta: float = 0.01,
+    loop_count: int = 10,
 ):
+    """
+    ksim_c.c の以下の関数を呼び出す。
+    void kuramot_model_simulator_c(
+        int n, double k, double *omega, double *theta,
+        int loop_count, double time_delta, double *com_x,
+        double *com_y) {
+    """
 
-    omega = (ctypes.c_double * n)(*[0.0] * n)
-    theta = (ctypes.c_double * n)(*[0.0] * n)
+    # input validation
+    for variable_name in ["input_omega", "input_theta"]:
+        if len(eval(variable_name)) != n:
+            raise ValueError(
+                f"Invalid list length: {variable_name} length is not {n}"
+            )
+        if not isinstance(eval(variable_name), list):
+            raise ValueError(
+                f"Invalid type: {variable_name} is not python list"
+            )
+
+    # make buffers for c library
+    omega = (ctypes.c_double * n)(*input_omega)
+    theta = (ctypes.c_double * n)(*input_theta)
     com_x = (ctypes.c_double * loop_count)(*[0.0] * loop_count)
     com_y = (ctypes.c_double * loop_count)(*[0.0] * loop_count)
 
+    # load c library
     ctypes.cdll.LoadLibrary(library_path)
     ksim = ctypes.CDLL(library_path)
 
+    # define argument types
     ksim.kuramoto_model_simulator_c.argtypes = [
         ctypes.c_int,  # n
         ctypes.c_double,  # k
-        ctypes.c_double,  # time_delta
-        ctypes.c_int,  # loop_count
-        ctypes.c_double,  # mu
-        ctypes.c_double,  # sigma
-        ctypes.c_uint,  # seed
         ctypes.POINTER(ctypes.c_double),  # *omega
         ctypes.POINTER(ctypes.c_double),  # *theta
+        ctypes.c_int,  # loop_count
+        ctypes.c_double,  # time_delta
         ctypes.POINTER(ctypes.c_double),  # *com_x
         ctypes.POINTER(ctypes.c_double),  # *com_y
-        ctypes.c_int,  # verbose
     ]
 
+    # call function
     ksim.kuramoto_model_simulator_c(
         n,
         k,
-        time_delta,
-        loop_count,
-        mu,
-        sigma,
-        seed,
         cast_double_p(omega),
         cast_double_p(theta),
+        loop_count,
+        time_delta,
         cast_double_p(com_x),
         cast_double_p(com_y),
-        verbose,
     )
+
+    # make result dict
+    result = {}
 
     params = [
         "library_path",
@@ -64,12 +105,8 @@ def kuramoto_model_simulator(
         "k",
         "time_delta",
         "loop_count",
-        "mu",
-        "sigma",
-        "seed",
-        "verbose",
     ]
-    result = {}
+
     result["params"] = {}
     for name in params:
         result["params"][name] = eval(name)
@@ -88,10 +125,9 @@ def kuramoto_model_simulator(
     "--ksim_library_path", type=click.Path(exists=True), default="./ksim.so"
 )
 def main(**kwargs):
+    omega, theta = sample_initial_values_from_normal_dist()
     result = kuramoto_model_simulator(
-        kwargs["ksim_library_path"],
-        loop_count=100,
-        n=50,
+        kwargs["ksim_library_path"], omega, theta
     )
     print(json.dumps(result))
 
